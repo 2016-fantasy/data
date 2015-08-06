c1277c41432e2787063eb7e235f5d7dd6f6e3ee9
Blake La Pierre <blakelapierre@gmail.com>
2015-08-05 19:22:43 -0700

    Map party codes to names. Pulled some FEC metadata.

--- src/index.js
+++ src/index.js
@@ -5,28 +5,29 @@ import _ from 'lodash';
 import promise from 'promise-callback';
 
 
-
+// http://www.fec.gov/finance/disclosure/metadata/metadataforcandidatesummary.shtml
+// Provide a mapping from the FEC's column names to ours (the 'to' field)
 const mapping = {
-  'lin_ima': 'url',
-  'can_id':  'fecId',
-  'can_nam': 'name',
-  'can_off': 'off?',
-  'an_off_sta': 'off_sta?',
-  'can_off_dis': 'off_dis?',
-  'can_par_aff': 'party',
-  'can_inc_cha_ope_sea': '??',
-  'can_str1': 'street1',
-  'can_str2': 'street2',
-  'can_cit': 'city',
-  'can_sta': 'state',
-  'can_zip': 'zip'//,
-  // 'ind_ite_con': '?',
-  // 'ind_uni_con': '?',
-  // 'ind_con': '?',
-  // 'par_com_con': '?',
+  'lin_ima': {to: 'url',   name: 'Link to list of reports for the committee', description: 'List of all disclosure filings for this committee'},
+  'can_id':  {to: 'fecId', name: 'Candidate Id'},
+  'can_nam': {to: 'name',  name: 'Candidate Name'},
+  'can_off': {to: 'office',name: 'Candidate Office'},
+  'can_off_sta': {to: 'officeState', name: 'Candidate Office State'},
+  'can_off_dis': {to: 'officeDistrict', name: 'Candidate Office District'},
+  'can_par_aff': {to: 'party', name: 'Candidate Party Affiliation'},
+  'can_inc_cha_ope_sea': {to: 'incumbentChallengerOpenSeat', name: 'Candidate Incumbent Challenger Open Seat'},
+  'can_str1': {to: 'street1', name: 'Candidate Street 1'},
+  'can_str2': {to: 'street2', name: 'Candidate Street 2'},
+  'can_cit':  {to: 'city', name: 'Candidate City'},
+  'can_sta':  {to: 'state',name: 'Candidate State'},
+  'can_zip':  {to: 'zip',  name: 'Candidate Zip'},
+  'ind_ite_con': {to: 'itemizedContributions', name: 'Individual Itemized Contribution'},
+  'ind_uni_con': {to: 'unitemizedContributions', name: 'Individual Unitemized Contribution'},
+  'ind_con': {to: 'individualContributions', name: 'Individual Contribution'},
+  'par_com_con': {to: 'partyCommitteeContribution', name: 'Party Committee Contribution'},
   // 'oth_com_con': '?',
   // 'can_con': '?',
-  // 'tot_con': '?',
+  'tot_con': 'totalContributions'
   // 'tra_fro_oth_aut_com': '?',
   // 'can_loa': '?',
   // 'oth_loa': '?',
@@ -59,8 +60,10 @@ const mapping = {
   // 'cov_end_dat': '?'
 };
 
-
+// After mapping names, each name will have their value transformed
+// with the following functions
 const transforms = {
+  // This parsing needs to be better. Some names have DR. MR. MRS, etc
   'name': name => {
     const parts = name.split(' '),
           [first] = parts,
@@ -82,30 +85,77 @@ const transforms = {
       display: parts.join(' '),
       parts
     };
-  }
+  },
+
+  'totalContributions': value => {
+    const [full, dollars, cents] = value.replace(/,/g, '')
+                         .replace(/\$/g, '')
+                         .match(/^(\d+)\.(\d+)$/);
+
+    return parseInt(dollars) * 100 + parseInt(cents);
+  },
+
+  'party': (() => {
+    const mapping = {
+      'REP': 'Republican',
+      'IND': 'Independent',
+      'DEM': 'Democrat',
+      'UNK': 'Unknown',
+      'OTH': 'Other',
+      'LRU': 'La Raza Unida',
+      'W':   'Write-In',
+      'NNE': 'None',
+      'UNI': 'United',
+      'IAP': 'Independent American',
+      'IDP': 'Independence',
+      'UN':  'Unaffiliated',
+      'AMP': 'American',
+      'NPA': 'No Party Affiliation',
+      'GRE': 'Green',
+      'FED': 'Federalist',
+      'CON': 'Constitution',
+      'LIB': 'Libertarian',
+      'CST': 'Constitutional',
+      'REF': 'Reform',
+      'NON': 'Non-Party',
+      'CRV': 'Conservative',
+      'DFL': 'Democratic-Farmer-Labor',
+      'ACE': 'Ace',
+      'AIP': 'American Independent',
+      'HEL': 'HEL',
+      'AME': 'AME',
+      'A99': 'A99',
+      'JCN': 'Jewish/Christian National',
+      'COM': 'Communist',
+      'NBC': 'NBC',
+      'PCH': 'Personal Choice'
+    };
+
+    return value => mapping[value] || value;
+  })()
 };
 
 promise(fs.readFile, `${__dirname}/../datasets/raw/fec/CandidateSummaryAction.csv`)
   .then(buffer => promise(csv.parse, buffer.toString(), {columns: true, relax: true}))
   .then(processRecords)
-  // .then(showRecords)
   .then(writeRecords)
-  .then(() => console.log('done!'))
+  .then(() => console.log('Candidates Done!'))
   .catch(error => console.error('Error', error.stack));
 
 function processRecords(records) {
   return  _.map(records, transformRecord);
 
   function transformRecord(record) {
-    return _.omit(
-            _.mapValues(
+    return _.mapValues(
+            _.omit(
               _.mapKeys(record, mapKeys),
-              mapValues
+              (value, key) => value === null || value === undefined || value === ''
             ),
-            (value, key) => value === null || value === undefined || value === '');
+            mapValues
+          );
 
     function mapKeys(value, key) {
-      return mapping[key] || key;
+      return (mapping[key] || {to: key}).to;
     }
 
     function mapValues(value, key) {
@@ -114,15 +164,6 @@ function processRecords(records) {
       return transform ? transform(value) : value;
     }
   }
-}
-
-function showRecords(records) {
-  console.log(records.length, 'records');
-  console.log('Showing first 2...');
-
-  _.each(_.take(records, 2), record => console.log(record));
-
-  return records;
 }
 
 function writeRecords(records) {
